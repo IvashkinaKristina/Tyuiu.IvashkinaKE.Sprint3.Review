5bfada73b388403d4eb9418b105765976755101c
Tyuiu.IvashkinaKE@outlook.com <Tyuiu.IvashkinaKE@outlook.com>
2023-11-01 08:59:21 +0500

    Add project files.

---
+++ Tyuiu.IvashkinaKE.Sprint3.TaskReview.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvashkinaKE.Sprint3.TaskReview.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvashkinaKE.Sprint3.TaskReview.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de1e76ba-519c-4bc6-81de-d2aae11acf1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvashkinaKE.Sprint3.TaskReview.V2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.IvashkinaKE.Sprint3.TaskReview.V2.Lib;
+
+namespace Tyuiu.IvashkinaKE.Sprint3.TaskReview.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Ивашкина К.Э. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: SprintReview                                                      *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Ивашкина Кристина Эдуардовна | АСОиУб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+            Console.WriteLine("*         cos x                                                           *");
+            Console.WriteLine("* F(x) = -------- - sin x * 12x + 2                                       *");
+            Console.WriteLine("*        x - 0,7                                                          *");
+            Console.WriteLine("* (произвести табулирование) F(x) на заданном диапазоне [-5; 5] с шагом 1 *");
+            Console.WriteLine("* При делениии на ноль вернуть значение 0.                                *");
+            Console.WriteLine("* Значеие округлить до двух знаков после запятой.                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага :" + startValue);
+            Console.WriteLine("Конец шага :" + stopValue);
+
+            int len = (stopValue - startValue) + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+            valueWaitArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+---------+---------+");
+            Console.WriteLine("|    X    |   f(x)  |");
+            Console.WriteLine("+---------+---------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}    | {1, 6:f2}  |", startValue, valueWaitArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+---------+---------+");
+
+            Console.ReadKey();
+        }
+    }
+}
